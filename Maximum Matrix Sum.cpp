c83f3bd22ba27130826374a244af82cece5368b6
Harleen <152802220+Harleenajimal@users.noreply.github.com>
2026-01-05 19:57:41 +0530

    Add maxMatrixSum function to calculate matrix sum
    Implement maxMatrixSum function to calculate the maximum matrix sum considering negative values.

---
+++ Maximum Matrix Sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long sumAbs = 0;
+        int negCount = 0;
+        int minAbs = INT_MAX;
+
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = matrix[i][j];
+                
+                if (val < 0) negCount++;
+                
+                int absVal = abs(val);
+                sumAbs += absVal;
+                minAbs = min(minAbs, absVal);
+            }
+        }
+
+        if (negCount % 2 == 0)
+            return sumAbs;
+        else
+            return sumAbs - 2LL * minAbs;
+    }
+};
