d23abe945dd1e3e1d5775cd550a930221838c449
Harleen <152802220+Harleenajimal@users.noreply.github.com>
2026-05-01 22:11:23 +0530

    Implement isMonotonic function for array check
    Fix logic errors and improve the isMonotonic function.

---
+++ Monotonic Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; i<nums.size();j++){
+                if(i<=j && nums[i] <= nums[j]) (i<=j && nums[i] >= nums[j])
+                bool True;
+            }
+        }
+        return bool;
+    }
+};
